be86c016f40fc0f300e02d08242e2c061b42f26f
Kostas Chaniotakis <kchaniot@c2s16d93.42wolfsburg.de>
2021-08-02 17:11:31 +0200

    sd

---
+++ born2beRoot/monitoring.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+arch=$(uname -a)
+
+wall "	
+	#Architecture: $arch
+	#CPU physical:
+	#CPU:
+	#Memory Usage:
+	#Disk Usage:
+	#CPU load:
+	#Last boot:
+	#LVM use:
+	#Connexions TCP:
+	#User Log:
+	#Network:
+	#SUdo:"
